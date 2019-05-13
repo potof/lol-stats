0f625c753457d217194785dd6cd5edc3ed1797b4
potof <potof6@gmail.com>
2019-05-13 22:18:25 +0900

    add csv

--- get-lolstats.py
+++ get-lolstats.py
@@ -20,13 +20,25 @@ class Player:
     death: int = 0
     assist: int = 0
     cs: int = 0
-    # statsに改行が入っている場合は消したい
     stats: dict = field(default_factory=dict)
+
+    def to_dict(self):
+        t = {"name":self.name, "champion":self.champion, "role":self.role, "kill":self.kill,
+                "death":self.death,"assist":self.assist, "cs": self.cs}
+        t.update(self.stats)
+        return t
+    
+    def set_kda(self, kda):
+         n = kda.split("/")
+         self.kill = n[0]
+         self.death = n[1]
+         self.assist = n[2]
+
 
 @dataclasses.dataclass
 class Team:
     game_result: str = ""
-    # roleを初期で登録したい
+    # TODO: player1~5は配列にしたほうがよさそう
     player1: Player = field(default_factory=Player)
     player2: Player = field(default_factory=Player)
     player3: Player = field(default_factory=Player)
@@ -43,15 +55,44 @@ class Team:
     ban4: str = ""
     ban5: str = ""
 
+    def to_csv(self):
+        cm = {"game_result":self.game_result, "towers_destroyed":self.towers_destroyed, "inhibitors_destroyed":self.inhibitors_destroyed,
+            "barons_slain":self.barons_slain, "dragons_slain":self.dragons_slain, "rift_heralds_slain":self.rift_heralds_slain,
+            "ban1":self.ban1, "ban2":self.ban2, "ban3":self.ban3, "ban4":self.ban4, "ban5":self.ban5}
+        p1 = self.player1.to_dict()
+        p2 = self.player2.to_dict()
+        p3 = self.player3.to_dict()
+        p4 = self.player4.to_dict()
+        p5 = self.player5.to_dict()
+        p1.update(cm)
+        p2.update(cm)
+        p3.update(cm)
+        p4.update(cm)
+        p5.update(cm)
+        return p1, p2, p3, p4, p5
+
+
 @dataclasses.dataclass
 class Game:
     date: str = ""
     time: str = ""
+    # TODO: team1, team2 は配列にしたほうがよさそう
     team1: Team = field(default_factory=Team)
     team2: Team = field(default_factory=Team)
 
-    def to_csv():
-        
+    def to_csv(self):
+        t1 = self.team1.to_csv()
+        t2 = self.team2.to_csv()
+        rs = []
+        for t in t1:
+            t.update({"date":self.date, "time":self.time})
+            rs.append(t)
+        for t in t2:
+            t.update({"date":self.date, "time":self.time})
+            rs.append(t)
+        return rs
+
+
 
 
 # TODO: Playerのメソッドにする
@@ -151,38 +192,54 @@ game.team2.player3.name = div_names[7].text
 game.team2.player4.name = div_names[8].text
 game.team2.player5.name = div_names[9].text
 
+div_champs = soup.select("div.player > div.champion-col > div > div.champion-nameplate > div > div > div")
+game.team1.player1.champion = div_champs[0].attrs["data-rg-id"]
+game.team1.player2.champion = div_champs[1].attrs["data-rg-id"]
+game.team1.player3.champion = div_champs[2].attrs["data-rg-id"]
+game.team1.player4.champion = div_champs[3].attrs["data-rg-id"]
+game.team1.player5.champion = div_champs[4].attrs["data-rg-id"]
+game.team2.player1.champion = div_champs[5].attrs["data-rg-id"]
+game.team2.player2.champion = div_champs[6].attrs["data-rg-id"]
+game.team2.player3.champion = div_champs[7].attrs["data-rg-id"]
+game.team2.player4.champion = div_champs[8].attrs["data-rg-id"]
+game.team2.player5.champion = div_champs[9].attrs["data-rg-id"]
+
+
+
 # プレイヤーの詳細statsを登録する
 # tr.grid-rowが選手ごとの情報を格納している形式ぽい
 rows = soup.select("tbody#stats-body > tr.grid-row")
 for row in rows:
 
     # 項目(div.viewで項目を表示しているぽい)
-    key = row.find("div", class_="view").string
+    key = row.find("div", class_="view").string.replace("\n","")
 
     # 値(div.grid-cellで値を表示しているぽい)
     value = row.find_all("div", class_="grid-cell")
 
     # player classのdictにいれる
     # TODO: プレイヤーごとに書かないような感じにしたい
-    game.team1.player1.stats[key] = convert_to_num(value[0].string)
-    game.team1.player2.stats[key] = convert_to_num(value[1].string)
-    game.team1.player3.stats[key] = convert_to_num(value[2].string)
-    game.team1.player4.stats[key] = convert_to_num(value[3].string)
-    game.team1.player5.stats[key] = convert_to_num(value[4].string)
-    game.team2.player1.stats[key] = convert_to_num(value[5].string)
-    game.team2.player2.stats[key] = convert_to_num(value[6].string)
-    game.team2.player3.stats[key] = convert_to_num(value[7].string)
-    game.team2.player4.stats[key] = convert_to_num(value[8].string)
-    game.team2.player5.stats[key] = convert_to_num(value[9].string)
+    game.team1.player1.stats[key] = convert_to_num(value[0].string.replace("\n",""))
+    game.team1.player2.stats[key] = convert_to_num(value[1].string.replace("\n",""))
+    game.team1.player3.stats[key] = convert_to_num(value[2].string.replace("\n",""))
+    game.team1.player4.stats[key] = convert_to_num(value[3].string.replace("\n",""))
+    game.team1.player5.stats[key] = convert_to_num(value[4].string.replace("\n",""))
+    game.team2.player1.stats[key] = convert_to_num(value[5].string.replace("\n",""))
+    game.team2.player2.stats[key] = convert_to_num(value[6].string.replace("\n",""))
+    game.team2.player3.stats[key] = convert_to_num(value[7].string.replace("\n",""))
+    game.team2.player4.stats[key] = convert_to_num(value[8].string.replace("\n",""))
+    game.team2.player5.stats[key] = convert_to_num(value[9].string.replace("\n",""))
+
 
 
 # CSV出力
 # output_file = "LJL-stats" + datetime.now().strftime("%Y%m%d-%H%M%S") + ".csv"
-# with open( output_file, "w", newline="") as f:
-#     writer = csv.writer(f)
+output_file = "LJL-stats.csv"
+with open( output_file, "w", newline="") as f:
+    csv_g = game.to_csv()
+    writer = csv.DictWriter(f, csv_g[0].keys())
+    writer.writeheader()
+    writer.writerows(csv_g)
 
-#     # for b in books_list:
-#     #     writer.writerow([b.readed_date, b.title,b.auther,b.page])
-#     #     print(b.title, b.auther, b.readed_date, b.page)
-# f.close()
+f.close()
 
